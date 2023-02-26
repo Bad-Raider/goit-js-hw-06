d22d42d44fedf9f0a0170876b8fc0a9f5a90aa77
Bad-Raider <stanislav.gusiev@gmail.com>
2023-02-26 18:56:50 +0200

    eighth
    task - 9 - ok

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,21 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// It`s function random color 
+const getRandomHexColor = () => {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+// It`s DOM eLements 
+const ref = {
+  body: document.querySelector("body"),
+  span: document.querySelector(".color"),
+  btn: document.querySelector(".change-color"),
+}
+// it`s hendler
+const handleChangeBodyColor = (event) => {
+  ref.body.style.backgroundColor = getRandomHexColor(event);
+  ref.span.textContent = getRandomHexColor(event);
+};
+
+ref.btn.addEventListener("click", handleChangeBodyColor);
+
+
