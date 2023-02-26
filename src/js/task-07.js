608ab70a1f084684f4e7ed78a73cf89bfc0ba3f9
Bad-Raider <stanislav.gusiev@gmail.com>
2023-02-26 18:18:28 +0200

    seven commit
    task - 8

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,1 +1,10 @@
-const
+// it`s link to the input element
+const inputEl = document.querySelector("#font-size-control");
+// it`s link to the span element
+const spanEL = document.querySelector("#text");
+// it`s hendler
+const handleSpanTextMakeSize = (event) => {
+    spanEL.style.fontSize = `${event.currentTarget.value}px`;
+};
+
+inputEl.addEventListener("input", handleSpanTextMakeSize);
--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,33 @@
+// It`s link to the form element
+const formEl = document.querySelector(".login-form");
+// it`s link to the input email
+const inputEmailEl = formEl.firstElementChild.firstElementChild;
+// it`s link to the input password
+const inputPasswordEl = formEl.children[1].firstElementChild;
+// it`s hendler
+const handleFormSubmit = (event) => {
+  event.preventDefault();
+  
+    if (inputEmailEl.value === "" || inputPasswordEl.value === "") {
+       return alert("Чуваче, заповни форму!")
+    }
+    const email = formEl.elements.email.value;
+    const password = formEl.elements.password.value;
+
+    const formData = {
+        email,
+        password,
+    }
+    console.log(formData);
+    event.currentTarget.reset();
+}
+
+formEl.addEventListener("submit", handleFormSubmit);
+
+
+
+
+
+
+
 
